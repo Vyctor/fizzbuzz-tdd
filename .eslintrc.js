d3ed0067241d1926de3af75a485f33480e46dce5
Vyctor <dev.vyctor@gmail.com>
2020-12-17 07:27:45 -0300

    configuracao inicial do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-unused-expressions': false,
+  },
+};
